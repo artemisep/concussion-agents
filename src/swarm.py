83f86902cd88ed8dc78b594ba1f4b50dd8513ef2
Ruihua <easymath00@gmail.com>
2024-07-14 02:29:32 -0700

    minor modification, check in slide

--- src/swarm.py
+++ src/swarm.py
@@ -13,13 +13,18 @@ flow = "Symptom-Agent -> Diagnosis-Agent -> Treatment-Agent -> Monitoring-Agent,
 # Using AgentRearrange class
 agent_system = AgentRearrange(agents=agents, flow=flow)
 output = agent_system.run("""
-                          Do this step by step, list the symptom of a student 
-                          whose head hit the wall
-                          give detailed step by step instruction for diagnosis as to whether
-                          the student have concussion and how severe it is. 
-                          Then provide detailed, step by step treatment plan, describe in detail on 
-                          what to monitor each day and how to manage recovery
+                          A student fell when playing basket ball.
+                          he has a headache, feels nausea and dizzy.
+                          He is also sensitive to light and sound and 
+                          feel sluggish.
+                          Do this step by step, 
+                          give detailed step by step instruction to diagnose
+                          as to whether the student have concussion and how 
+                          severe it is. 
+                          Then provide detailed, step by step treatment plan, 
+                          and give detailed, step by step procedures on what 
+                          to monitor each day and how to manage recovery
                           in this student's 
-                          daily life and school activities
+                          daily life and school activities. 
                           """)
 print(output)
